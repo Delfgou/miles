6777c4d57675e83a7bd23bf0be50ec09db5677e7
cde <cyrill.delfgou@elca.ch>
2021-06-16 11:49:09 +0200

    trying iframe

--- src/app/bands/wolfer/wolfer.component.ts
+++ src/app/bands/wolfer/wolfer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-wolfer',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wolfer.component.css']
 })
 export class WolferComponent implements OnInit {
+  safeURL;
+  videoURL = 'https://soundcloud.com/dadifreyr/think-about-things';
+  // videoURL = 'https://www.youtube.com/watch?v=VFZNvj-HfBU';
 
-  constructor() { }
+  constructor(private _sanitizer: DomSanitizer){
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+ }
 
   ngOnInit(): void {
   }
