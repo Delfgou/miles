7f92a740e7028f8e1010c53abca5ef1af9e51c52
cde <cyrill.delfgou@elca.ch>
2021-06-18 12:29:18 +0200

    added pdf download

--- src/app/bands/against-the-void/against-the-void.component.ts
+++ src/app/bands/against-the-void/against-the-void.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgainstTheVoidComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  public download() {
+    this.http.get('../assets/documents/miles_doc.pdf', {responseType: 'blob'})
+    .subscribe(response => {
+      this.downLoadFile(response, "application/pdf")
+    });
+  }
+
+  downLoadFile(data: any, type: string) {
+    let blob = new Blob([data], { type: type});
+    let url = window.URL.createObjectURL(blob);
+    window.open(url);
+  }
 }
